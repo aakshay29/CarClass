a7213554b75d5c09e739f517cf05a42d77449a36
aakshay29 <aakshay.29@gmail.com>
2016-09-08 14:18:12 -0400

    seventh commit

--- workspace/Zork/src/Zork.java
+++ workspace/Zork/src/Zork.java
@@ -24,6 +24,7 @@ public class Zork {
 	static int lampRoom = 0;
 	static String stash = "";
 	static String pathFinder = "";
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Zork.");
@@ -46,23 +47,25 @@ public class Zork {
 
 	public static void checkLamp(int roomNumber) {
 		if (roomNumber == lampRoom && hasLamp == false) {
-			Scanner sc = new Scanner(System.in);
+
 			System.out.println(
-					"There is a lamp in this room. You can see the rooms better with the lamp. Type \"Get Lamp\" to get it.");
+					"There is a lamp in this room. You can see the rooms better with the lamp. Type \"GetLamp\" to get it.");
 			String response = sc.nextLine();
-			if (response.equalsIgnoreCase("Get Lamp")) {
+			if (response.equalsIgnoreCase("GetLamp")) {
 				hasLamp = true;
 			} else {
 				System.out.println("You did not pick up the lamp.");
 			}
-			sc.close();
+
+		} else {
+			// do nothing
 		}
 	}
 
 	public static void illuminatedRoom(int roomNumber) {
 		if (roomNumber == 1 && hasLamp == true && room2WithLamp == false) {
 			System.out.println("it has a spider web made from pure silk containing gold coins. \"Pick up\".");
-			Scanner sc = new Scanner(System.in);
+
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Pick Up")) {
 				room1WithLamp = true;
@@ -70,23 +73,22 @@ public class Zork {
 			} else {
 				System.out.println("You did not take the gold coins.");
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 2 && hasLamp == true && room2WithLamp == false) {
 			System.out.println("The piano has the sheet music for your favorite song. \"Play\"?");
-			Scanner sc = new Scanner(System.in);
-			String response = sc.nextLine();
+			String response = sc.next();
 			if (response.equalsIgnoreCase("Play")) {
 				room2WithLamp = true;
 				stash += "Piano. ";
 			} else {
 				System.out.println("You did not play the piano.");
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 3 && hasLamp == true && room3WithLamp == false) {
-			System.out.println("You see a scret message on the wall. \"Inspect Scroll\"");
-			Scanner sc = new Scanner(System.in);
+			System.out.println("You see a secret message on the wall. \"Inspect Scroll\"");
+
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Inspect Scroll")) {
 				System.out.println("It says \"What comes down but never goes up?\" What is your answer?");
@@ -105,30 +107,29 @@ public class Zork {
 					System.out.println("Your answer was incorrect! Re-enter this room to play again.");
 				}
 			}
-			sc.close();
+
 		} else if (roomNumber == 3 && hasLamp == false) {
-			Scanner sc = new Scanner(System.in);
+
 			System.out.println(
-					"You are standing in the library. You see a bunch of spiders. {You can (1)exit to the east or (2)exit to the north}");
-			int response = sc.nextInt();
-			if (response == 1) {
+					"You are standing in the library. You see a bunch of spiders. {You can go \"east\" to the \"front room\" or \"north\" to the \"dining room\"}");
+			String response = sc.nextLine();
+			if (response.equalsIgnoreCase("east") || response.equalsIgnoreCase("front room")) {
 				pathFinder += "\nYou went to the front room. ";
 				payMoney(2);
 				checkForBurglar(2);
 				checkLamp(2);
 				visitRoom2();
-			} else {
+			} else if (response.equalsIgnoreCase("north") || response.equalsIgnoreCase("dining room")) {
 				pathFinder += "\nYou entered the Dining Room. ";
 				payMoney(5);
 				checkForBurglar(5);
 				checkLamp(5);
 				visitRoom5();
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 4 && hasLamp == true && room4WithLamp == false) {
 			System.out.println("You find food there. \"Drink Coke\" or \"Eat Chips\"");
-			Scanner sc = new Scanner(System.in);
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Drink Coke")) {
 				room4WithLamp = true;
@@ -139,11 +140,11 @@ public class Zork {
 			} else {
 				System.out.println("You did not eat anything.");
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 5 && hasLamp == true && room5WithLamp == false) {
 			System.out.println("The box has an Amazon Gift Card. \"Get Gift Card\"?");
-			Scanner sc = new Scanner(System.in);
+
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Get Gift Card")) {
 				room5WithLamp = true;
@@ -151,11 +152,11 @@ public class Zork {
 			} else {
 				System.out.println("You did not take the gift card.");
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 7 && hasLamp == true && room7WithLamp == false) {
+
 			System.out.println("This place has a poster of Tom Hanks with 5 tickets to Sully next week. \"Pick Up\"?");
-			Scanner sc = new Scanner(System.in);
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Pick Up")) {
 				room7WithLamp = true;
@@ -163,11 +164,11 @@ public class Zork {
 			} else {
 				System.out.println("You did not pick up the stuff.");
 			}
-			sc.close();
+
 		}
 		if (roomNumber == 8 && hasLamp == true && room8WithLamp == false) {
 			System.out.println("You found a map of the house. \"Pick Up\"?");
-			Scanner sc = new Scanner(System.in);
+
 			String response = sc.nextLine();
 			if (response.equalsIgnoreCase("Pick Up")) {
 				room8WithLamp = true;
@@ -175,7 +176,7 @@ public class Zork {
 			} else {
 				System.out.println("You did not pick up the map.");
 			}
-			sc.close();
+
 		}
 	}
 
@@ -255,17 +256,17 @@ public class Zork {
 
 	public static void visitRoom1() {
 		illuminatedRoom(1);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are standing in the foyer of an old house. You see a dead scorpion. {You can (1)exit to the north or press (2) to quit}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You are standing in the foyer of an old house. You see a dead scorpion. {You can go \"north\" to the \"front room\" or \"quit\"}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("north") || response.equalsIgnoreCase("front room")) {
 			pathFinder += "\nYou went to the front room.";
 			payMoney(2);
 			checkForBurglar(2);
 			checkLamp(2);
 			visitRoom2();
-		} else {
+		} else if (response.equalsIgnoreCase("quit")) {
 			Random rnd = new Random();
 			int randomInt = 1 + rnd.nextInt(4);
 			if (randomInt == 1) {
@@ -273,38 +274,38 @@ public class Zork {
 			} else {
 				System.out.println("You have exited the house, stay safe!");
 			}
-			System.out.println("\nLog: " + pathFinder + "\n\nYour total money: " + totalMoney + "\n\nYour stash: " + stash);
+			System.out.println(
+					"\nLog: " + pathFinder + "\n\nYour total money: " + totalMoney + "\n\nYour stash: " + stash);
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom2() {
 		illuminatedRoom(2);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are in the front room and you see a piano. {You can (1) exit to the south or (2) exit to the west or (3) exit to the east}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You are in the front room and you see a piano. {You can go \"south\" to the \"Foyer\" or go \"west\" to the \"Library\" or go \"east\" to the \"kitchen\"}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("south") || response.equalsIgnoreCase("Foyer")) {
 			pathFinder += "\nYou entered the Foyer and saw dead scorpions. ";
 			System.out.println("Your total money now is: $" + totalMoney);
 			checkForBurglar(1);
 			checkLamp(1);
 			visitRoom1();
-		}
-		if (response == 2) {
+		} else if (response.equalsIgnoreCase("west") || response.equalsIgnoreCase("Library")) {
 			pathFinder += "\nYou entered the Library and saw spiders.";
 			payMoney(3);
 			checkForBurglar(3);
 			checkLamp(3);
 			visitRoom3();
-		} else {
+		} else if (response.equalsIgnoreCase("east") || response.equalsIgnoreCase("kitchen")) {
 			pathFinder += "\nYou entered the Kitchen and saw bats. ";
 			payMoney(4);
 			checkForBurglar(4);
 			checkLamp(4);
 			visitRoom4();
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom3() {
@@ -313,39 +314,39 @@ public class Zork {
 
 	public static void visitRoom4() {
 		illuminatedRoom(4);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are standing in the kitchen. You see a lot of bats flying. {You can (1)exit to the west or (2)exit to the north}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You are standing in the kitchen. You see a lot of bats flying. {You can go \"west\" to the \"front room\" or go \"north\" to the \"parlor\"}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("west") || response.equalsIgnoreCase("front room")) {
 			pathFinder += "\nYou went to the front room.";
 			payMoney(2);
 			checkForBurglar(2);
 			checkLamp(2);
 			visitRoom2();
-		} else {
+		} else if (response.equalsIgnoreCase("north") || response.equalsIgnoreCase("parlor")) {
 			pathFinder += "\nYou entered the Parlor and saw a treasure chest. ";
 			payMoney(7);
 			checkForBurglar(7);
 			checkLamp(7);
 			visitRoom7();
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom5() {
 		illuminatedRoom(5);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are standing in the dining room. You see a lot of dust and an empty box. {You can (1)exit to the south or (2)exit the game}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You are standing in the dining room. You see a lot of dust and an empty box. {You can go \"south\" to the \"library\" or \"exit\" the game}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("south") || response.equalsIgnoreCase("library")) {
 			pathFinder += "\nYou entered the Library and saw spiders. ";
 			payMoney(3);
 			checkForBurglar(3);
 			checkLamp(3);
 			visitRoom3();
-		} else {
+		} else if (response.equalsIgnoreCase("quit")) {
 			Random rnd = new Random();
 			int randomInt = 1 + rnd.nextInt(8);
 			if (randomInt == 1 || randomInt == 3 || randomInt == 5 || randomInt == 7) {
@@ -364,16 +365,16 @@ public class Zork {
 				visitRoom2();
 			}
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom6() {
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are standing in the vault room. You see 3 walking skeletons. {You can quickly (1)exit to the east or (2) exit to north}");
-		int response = sc.nextInt();
+				"You are standing in the vault room. You see 3 walking skeletons. {You can quickly go \"east\" or go \"north\"}");
+		String response = sc.nextLine();
 		if (unlocked = false) {
-			if (response == 1) {
+			if (response.equalsIgnoreCase("east")) {
 				Random rnd = new Random();
 				int randomInt = 1 + rnd.nextInt(4);
 				if (randomInt == 1) {
@@ -389,8 +390,7 @@ public class Zork {
 					checkLamp(7);
 					visitRoom7();
 				}
-			}
-			else if (response == 2) {
+			} else if (response.equalsIgnoreCase("north")) {
 				System.out.println("What? You came to room 2!");
 				pathFinder += "\nYou went to the front room.";
 				payMoney(2);
@@ -398,52 +398,51 @@ public class Zork {
 				checkLamp(2);
 				visitRoom2();
 			}
-		} 
-		else {
+		} else {
 			pathFinder += "\nYou entered the Secret Room and see piles of gold. ";
 			payMoney(8);
 			checkForBurglar(8);
 			checkLamp(8);
 			visitRoom8();
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom7() {
 		illuminatedRoom(7);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You are standing in the parlor. You see a treasure chest. {You can (1)exit to the west or (2)exit to the south}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You are standing in the parlor. You see a treasure chest. {You can go \"west\" to the \"Vault\" or go \"south\" to the \"kitchen\"}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("west") || response.equalsIgnoreCase("vault")) {
 			pathFinder += "\nYou entered the Vault and see 3 walking skeletons. ";
 			payMoney(6);
 			checkForBurglar(6);
 			checkLamp(6);
 			visitRoom6();
-		} else {
+		} else if (response.equalsIgnoreCase("south") || response.equalsIgnoreCase("kitchen")) {
 			pathFinder += "\nYou entered the Kitchen and see bats. ";
 			payMoney(4);
 			checkForBurglar(4);
 			checkLamp(4);
 			visitRoom4();
 		}
-		sc.close();
+
 	}
 
 	public static void visitRoom8() {
 		illuminatedRoom(8);
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println(
-				"You find the secret room. You see piles of gold. {You can (1)exit to the west with the gold or (2)exit the game without the gold}");
-		int response = sc.nextInt();
-		if (response == 1) {
+				"You find the secret room. You see piles of gold. {You can go \"west\" to the \"vault\" with the gold or \"quit\" the game without the gold}");
+		String response = sc.nextLine();
+		if (response.equalsIgnoreCase("west") || response.equalsIgnoreCase("vault")) {
 			pathFinder += "\nYou entered the Vault and see 3 walking skeletons. ";
 			payMoney(6);
 			checkForBurglar(6);
 			checkLamp(6);
 			visitRoom6();
-		} else {
+		} else if (response.equalsIgnoreCase("quit")) {
 			Random rnd = new Random();
 			int randomInt = 1 + rnd.nextInt(4);
 			if (randomInt == 1) {
@@ -451,8 +450,9 @@ public class Zork {
 			} else {
 				System.out.println("You have exited the house, stay safe!");
 			}
-			System.out.println("\nLog: " + pathFinder + "\n\nYour total money: " + totalMoney + "\n\nYour stash: " + stash);
+			System.out.println(
+					"\nLog: " + pathFinder + "\n\nYour total money: " + totalMoney + "\n\nYour stash: " + stash);
 		}
-		sc.close();
+
 	}
 }
