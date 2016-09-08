0efcec159e74a765a420c84fc0a1593e26970343
aakshay29 <aakshay.29@gmail.com>
2016-09-08 15:55:34 -0400

    second commit

--- workspace/InvoiceArray/src/InvoiceArray.java
+++ workspace/InvoiceArray/src/InvoiceArray.java
@@ -1,7 +1,9 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class InvoiceArray {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the invoice application. Enter numbers to fill the billing and tax information. Enter any other number to break out.");
 		Scanner scanner = new Scanner(System.in);
 		double total = 0; 
 		int number = 0;
@@ -11,7 +13,20 @@ public class InvoiceArray {
 		for(int i = 0; i < 100; i++){
 			number = i+1;
 			System.out.println("Price #" + number);
-			double nextPrice = scanner.nextDouble();
+			double nextPrice = 0;
+			try{
+				nextPrice = scanner.nextDouble();		
+			}
+			catch(InputMismatchException Ex){
+				System.out.println("End of input");
+				break;
+			}
+			catch(Exception Ex){
+				System.out.println(Ex.getMessage());
+			}
+			finally{
+				System.out.println("Thank you");
+			}
 			if(nextPrice == 0)
 				break;
 			myPrices[i] = nextPrice;
