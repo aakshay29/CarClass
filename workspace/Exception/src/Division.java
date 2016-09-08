3b501b719a171c2f16c2c1170d589c72c73c7c54
aakshay29 <aakshay.29@gmail.com>
2016-09-08 15:02:42 -0400

    first commit

---
+++ workspace/Exception/src/Division.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Division {
+	public static void main(String[] args) {
+
+		int a, b, result = 0;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input two integers");
+
+		a = input.nextInt();
+		b = input.nextInt();
+		try {
+			result = a / b;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+
+		System.out.println("Result = " + result);
+
+		input.close();
+	}
+}
