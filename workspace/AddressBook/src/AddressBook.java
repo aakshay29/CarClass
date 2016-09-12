86f179a33b1d34a1e5519b8e46fef65aca7e7fea
aakshay29 <aakshay.29@gmail.com>
2016-09-12 10:16:15 -0400

    first commit

---
+++ workspace/AddressBook/src/AddressBook.java
@@ -0,0 +1,90 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AddressBook {
+
+	static String name;
+	static String address;
+	static String city;
+	static String state;
+	static String zip;
+	static String phone;
+	static String email;
+
+	public void setName(String inputName){
+		name = inputName;
+	}	
+	public String getName(){
+		return name;
+	}
+	
+	public void setAddress(String inputAddress){
+		address = inputAddress;
+	}	
+	public String getAddress(){
+		return address;
+	}
+	
+	public void setCity(String inputCity){
+		city = inputCity;
+	}	
+	public String getCity(){
+		return city;
+	}
+	
+	public void setState(String inputState){
+		state = inputState;
+	}	
+	public String getState(){
+		return state;
+	}
+	
+	public void setZip(String inputZip){
+		zip = inputZip;
+	}	
+	public String getZip(){
+		return zip;
+	}
+	
+	public void setPhone(String inputPhone){
+		phone = inputPhone;
+	}	
+	public String getPhone(){
+		return phone;
+	}
+	
+	public void setEmail(String inputEmail){
+		email = inputEmail;
+	}	
+	public String getEmail(){
+		return email;
+	}
+	
+	public boolean validateEmail(String emailAddress) {
+		boolean isValid = false;
+		String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+		CharSequence inputStr = emailAddress;
+		// Make the comparison case-insensitive.
+		Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(inputStr);
+		if (matcher.matches()) {
+
+			isValid = true;
+		}
+		return isValid;
+	}
+
+	public boolean validatePhone(String phoneNumber) {
+		boolean isValid = false;
+
+		String expression = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
+		CharSequence inputStr = phoneNumber;
+		Pattern pattern = Pattern.compile(expression);
+		Matcher matcher = pattern.matcher(inputStr);
+		if (matcher.matches()) {
+			isValid = true;
+		}
+		return isValid;
+	}
+
+}
---
+++ workspace/AddressBook/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		AddressBook ab = new AddressBook();
+		print("Enter Name: ");
+		ab.setName(sc.nextLine());
+		print("Enter Address: ");
+		ab.setAddress(sc.nextLine());
+		print("Enter City: ");
+		ab.setCity(sc.nextLine());
+		print("Enter State: ");
+		ab.setState(sc.nextLine());
+		print("Enter Zip: ");
+		ab.setZip(sc.nextLine());
+		print("Enter Phone: ");
+		String phone = sc.nextLine();
+		boolean validPhone = ab.validatePhone(phone);
+		print("Enter Email: ");
+		String email = sc.nextLine();
+		boolean validEmail = ab.validateEmail(email);
+		
+		if(validPhone == true){
+			ab.setPhone(phone);
+		}
+		else{
+			print("Invalid Phone Number\n");
+		}
+		if(validEmail == true){
+			ab.setEmail(email);
+		}
+		else{
+			print("Invalid Email Address\n");
+		}
+		
+		print("Name: " + ab.getName());
+		print("Address: " + ab.getAddress());
+		print("City: " + ab.getCity());
+		print("State: " + ab.getState());
+		print("Zip: " + ab.getZip());
+		print("Phone: " + ab.getPhone());
+		print("Email: " + ab.getEmail());
+	}
+	public static void print(String input){
+		System.out.println(input);
+	}
+}
