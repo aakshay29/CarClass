fbf2f583d100b6278de9a9dd6713310b3c0a1e8b
aakshay29 <aakshay.29@gmail.com>
2016-09-12 11:05:10 -0400

    first commit

---
+++ workspace/CompositionBasics/src/CompositionBasics.java
@@ -0,0 +1,16 @@
+
+public class CompositionBasics {
+	public static void main(String[] args) {
+		Person bobby = new Person();
+		System.out.println(bobby.getSalary());
+		
+		Job job = new Job();		
+		job.setId(1);
+		job.setRole("Analyst");
+		job.setSalary(2500000);
+		System.out.println(job.toString());
+		
+		Education education = new Education();
+		System.out.println(education.toString());
+	}
+}
---
+++ workspace/CompositionBasics/src/Education.java
@@ -0,0 +1,8 @@
+
+public class Education {
+	static String lastSchools = "Oxford, Yale, Princeton, UofI, Brown, CMU, USC, UCDavis, UCBerkeley, TAMU";
+	@Override
+	public String toString(){	
+		return lastSchools;
+	}
+}
